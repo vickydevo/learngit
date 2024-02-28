04844d1c4f669d17779fdbbf3a95bc132d994e02
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2024-02-28 07:04:41 +0530

    add

--- introduction.sh
+++ introduction.sh
@@ -1,7 +1,25 @@
 GIT (Global Information Tracker)
+
+https://www.youtube.com/watch?v=LGLuHYHbCFA  look into it
 git, github
 -----------
 1.VCS & GIT Repos
 2.staging area and commit
 3. Branching
-4. issues and pull request
+4. issues and pull request (PRs)
+
+Deployment is the mechanism through which applications, modules, 
+  updates, and patches are delivered from developers to users.
+
+What Is Deployment in DevOps? 
+Deployment in DevOps is a process that enables you to retrieve important codes from version control
+ so that they can be made readily available to the public and 
+ they can use the application in a ready-to-use and automated fashion.
+
+  project - website
+  -------------------
+
+    code changes --> redeploy
+    we get bugs (unexpected error)
+    traffic --> decrease ---> because user experince (ux)
+    we revert back to pervious code some version 1.5.2
