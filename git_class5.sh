9eace53ad7d922becf01eda2bf60acf42784faaf
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2024-09-18 15:04:59 +0530

    Update git_class5.sh

--- git_class5.sh
+++ git_class5.sh
@@ -2,14 +2,11 @@ Date: 18/09/2024  Monday
 Batch: @7
 Class DEVOPS  [morning]  : 8am - 2:00pm
 -------------------------------------------
-
 ssh-coonectivity 
 ----------------
 ssh-keygen   -->  private and public key are created in pc/ec2 
             --> put id_rsa.pub [public] in github settings--> ssh-keys 
             --> then copy git cli url for specific repo 
-
-
 
 #  pull request:
 #  -------------
@@ -26,14 +23,12 @@ ssh-keygen   -->  private and public key are created in pc/ec2
      
 git revert:
 -----------
-
 Purpose: git revert is used to [ create a new commit] that undoes the changes introduced by a previous commit. 
     It does this by creating a new commit that applies the "reverse" of the changes made in the specified commit.
     Effects: git revert does not modify existing commits; instead, it creates a new commit with the reverted changes. This preserves the commit history and is safer for collaborative workflows.
         git revert 98bd832 
 git reset:
 ----------
-
 Purpose: git reset is used to move the HEAD pointer to a different commit, 
         effectively resetting the current branch to that commit. 
         It can also be used to unstage changes that have been added to the staging area.
